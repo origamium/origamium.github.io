38daf5101a4070e83e6c6381a1ed1d912ee96ab2
ONOUE Origami <yonosamochi@me.com>
2018-02-11 12:30:51 +0900

    それっぽいことに

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Body = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Container = styled.div`
+    background: azure;
+    width: 100px;
+    height: 100px;
+`;
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Body>
+                <Container>
+                </Container>
+            </Body>
+        );
+    }
 }
 
 export default App;
