16f5a1151ede0f5978769be8ac02fb5acd968f5e
ONOUE Origami <yonosamochi@me.com>
2018-02-17 02:22:35 +0900

    カードをクリックしたら裏面を表示するように

--- src/Card.js
+++ src/Card.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react';
 import styled from 'styled-components';
 
 import Front from './Front';
+import Back from './Back';
 
 const Body = styled.div`
     width: 459px;
@@ -11,16 +12,27 @@ const Body = styled.div`
     padding: 6px;
     filter: drop-shadow(2px 2px 2px rgba(0,0,0,0.6));
     transition: all 0.2s ease-out 0s;
+    
     &:hover {
         filter: drop-shadow(4px 4px 4px rgba(0,0,0,0.6));
     }
 `;
 
 class Card extends PureComponent {
+    state = {
+        alt: true
+    };
+
+    handleCardClick = () => {
+        this.setState({
+            alt: !this.state.alt
+        })
+    };
+
     render() {
         return (
-            <Body>
-                <Front/>
+            <Body onClick={this.handleCardClick}>
+            {this.state.alt ? <Front/> : <Back/>}
             </Body>
         );
     }
