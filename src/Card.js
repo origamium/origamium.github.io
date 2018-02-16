9eb3c2ee2b74765c147ef16f49b17c6ab97198fe
ONOUE Origami <yonosamochi@me.com>
2018-02-17 02:08:57 +0900

    ホバーすると浮いて見えるように クリックできるみたいでしょう？

--- src/Card.js
+++ src/Card.js
@@ -8,12 +8,17 @@ const Body = styled.div`
     border-radius: 2px;
     padding: 6px;
     filter: drop-shadow(2px 2px 2px rgba(0,0,0,0.6));
+    transition: all 0.2s ease-out 0s;
+    &:hover {
+        filter: drop-shadow(4px 4px 4px rgba(0,0,0,0.6));
+    }
 `;
 
 const Name = styled.p`
     font-family: 'Lato', sans-serif;
     font-weight: 800;
     font-size: 32px;
+    user-select: none;
     letter-spacing: 4px;
     color: grey;
 `;
