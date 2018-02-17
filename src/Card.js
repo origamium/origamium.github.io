077e432a8898c2b9657deb8991cc8cd39f4135b0
on_origami <yonosamochi@me.com>
2018-02-17 16:36:03 +0900

    影の作り方を微妙に変更

--- src/Card.js
+++ src/Card.js
@@ -11,10 +11,10 @@ const Body = styled.div`
     border-radius: 2px;
     padding: 6px;
     filter: drop-shadow(2px 2px 2px rgba(0,0,0,0.6));
-    transition: all 0.2s ease-out 0s;
+    transition: all 0.2s ease-in-out;
     
     &:hover {
-        filter: drop-shadow(4px 4px 4px rgba(0,0,0,0.6));
+        filter: drop-shadow(4px 4px 6px rgba(0,0,0,0.6));
     }
 `;
 
