5f7695df96f78e3a03f4b4f19bc12832c49bc9f7
ONOUE Origami <yonosamochi@me.com>
2018-03-06 04:14:10 +0900

    吉野家コピペから自分のリンク集へ

--- src/Back.js
+++ src/Back.js
@@ -19,43 +19,43 @@ const Body = styled.div`
     }
 `;
 
-const Text = styled.p`
+const Text = styled.div`
     font-family: 'Lato', sans-serif;
     font-weight: 400;
-    font-size: 16px;
+    font-size: 18px;
     user-select: none;
     letter-spacing: 1px;
     color: grey;
+    
+    & > pre > a {
+        &:link, &:visited {
+            color: grey;
+        }
+    }
 `;
+const content =
+<pre>{`{
+    "name": "ONOUE Origami",
+    "github": "`}<a href="https://github.com/FirstStar">FirstStar</a>{`",
+    "social": [
+        {"twitter.com": "`}<a href="https://twitter.com/arclisp">arclisp</a>{`"},
+        {"freezepeach.xyz": "`}<a href="https://freezepeach.xyz/origami">origami</a>{`"},
+        {"pawoo.net": "`}<a href="https://pawoo.net/@origami">origami</a>{`"},
+        {"mstdn.jp": "`}<a href="https://mstdn.jp/@lisp">lisp</a>{`"}
+    ],
+    "blog": [
+        {"note.mu": "`}<a href="https://note.mu/garbageable">garbageable</a>{`"},
+        {"dev.to": "`}<a href="https://dev.to/firststar">firststar</a>{`"},
+        {"medium.com": "`}<a href="https://medium.com/@schemelisp">@schemelisp</a>{`"}
+    ]
+}`}</pre>;
+
 
 const Front = (props) => (
     <Body>
-    <Text>昨日、近所の吉野家行ったんです。吉野家。
-        そしたらなんか人がめちゃくちゃいっぱいで座れないんです。
-        で、よく見たらなんか垂れ幕下がってて、１５０円引き、とか書いてあるんです。
-        もうね、アホかと。馬鹿かと。
-        お前らな、１５０円引き如きで普段来てない吉野家に来てんじゃねーよ、ボケが。
-        １５０円だよ、１５０円。
-        なんか親子連れとかもいるし。一家４人で吉野家か。おめでてーな。
-        よーしパパ特盛頼んじゃうぞー、とか言ってるの。もう見てらんない。
-        お前らな、１５０円やるからその席空けろと。
-        吉野家ってのはな、もっと殺伐としてるべきなんだよ。
-        Ｕの字テーブルの向かいに座った奴といつ喧嘩が始まってもおかしくない、
-        刺すか刺されるか、そんな雰囲気がいいんじゃねーか。女子供は、すっこんでろ。
-        で、やっと座れたかと思ったら、隣の奴が、大盛つゆだくで、とか言ってるんです。
-        そこでまたぶち切れですよ。
-        あのな、つゆだくなんてきょうび流行んねーんだよ。ボケが。
-        得意げな顔して何が、つゆだくで、だ。
-        お前は本当につゆだくを食いたいのかと問いたい。問い詰めたい。小１時間問い詰めたい。
-        お前、つゆだくって言いたいだけちゃうんかと。
-        吉野家通の俺から言わせてもらえば今、吉野家通の間での最新流行はやっぱり、
-        ねぎだく、これだね。
-        大盛りねぎだくギョク。これが通の頼み方。
-        ねぎだくってのはねぎが多めに入ってる。そん代わり肉が少なめ。これ。
-        で、それに大盛りギョク（玉子）。これ最強。
-        しかしこれを頼むと次から店員にマークされるという危険も伴う、諸刃の剣。
-        素人にはお薦め出来ない。
-        まあお前らド素人は、牛鮭定食でも食ってなさいってこった。</Text>
+    <Text>
+        {content}
+    </Text>
     </Body>
 );
 
